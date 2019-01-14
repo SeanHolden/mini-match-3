81ee41777f6d72868e09f4a58b5cfe115a38365b
Sean Holden <sean.holden@bbc.co.uk>
2019-01-14 15:58:50 +0000

    Refactored rotated-grid test

--- test/rotated-grid-coords.test.js
+++ test/rotated-grid-coords.test.js
@@ -4,47 +4,7 @@ const ary = [
   [3, 5, 4, 1, 2],
 ]
 
-const monkey = (coords) => {
-  if (coords.x === 0 && coords.y === 0) {
-    return ary[0][4]
-  }
-
-  if (coords.x === 0 && coords.y === 1) {
-    return ary[0][3]
-  }
-
-  if (coords.x === 0 && coords.y === 2) {
-    return ary[0][2]
-  }
-
-  if (coords.x === 0 && coords.y === 3) {
-    return ary[0][1]
-  }
-
-  if (coords.x === 0 && coords.y === 4) {
-    return ary[0][0]
-  }
-
-  if (coords.x === 1 && coords.y === 0) {
-    return ary[1][4]
-  }
-
-  if (coords.x === 1 && coords.y === 1) {
-    return ary[1][3]
-  }
-
-  if (coords.x === 1 && coords.y === 2) {
-    return ary[1][2]
-  }
-
-  if (coords.x === 1 && coords.y === 3) {
-    return ary[1][1]
-  }
-
-  if (coords.x === 1 && coords.y === 4) {
-    return ary[1][0]
-  }
-}
+const monkey = coords => ary[coords.x][(ary[0].length - 1) - coords.y]
 
 describe('rotatedGridCoords', () => {
   test('entering 0,0 returns actual value of 0,4', () => {
@@ -95,5 +55,30 @@ describe('rotatedGridCoords', () => {
   test('entering 1,4 returns actual value of 1,0', () => {
     const coords = { x: 1, y: 4 }
     expect(monkey(coords)).toEqual(2)
+  })
+
+  test('entering 2,0 returns actual value of 2,4', () => {
+    const coords = { x: 2, y: 0 }
+    expect(monkey(coords)).toEqual(2)
+  })
+
+  test('entering 2,1 returns actual value of 2,3', () => {
+    const coords = { x: 2, y: 1 }
+    expect(monkey(coords)).toEqual(1)
+  })
+
+  test('entering 2,2 returns actual value of 2,2', () => {
+    const coords = { x: 2, y: 2 }
+    expect(monkey(coords)).toEqual(4)
+  })
+
+  test('entering 2,3 returns actual value of 2,1', () => {
+    const coords = { x: 2, y: 3 }
+    expect(monkey(coords)).toEqual(5)
+  })
+
+  test('entering 2,4 returns actual value of 2,0', () => {
+    const coords = { x: 2, y: 4 }
+    expect(monkey(coords)).toEqual(3)
   })
 })
